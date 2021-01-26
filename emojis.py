18ffe77fb4ab87d978648041b01bbe7a9ea301b6
Monika Kubowicz <monkubowicz@gmail.com>
2021-01-26 19:31:21 +0100

    Added a py file with emojis unicodes

---
+++ emojis.py
@@ -0,0 +1,35 @@
+
+
+#PLAYER:
+pregnant_blonde = print("\U0001F930")
+breastfeeding_blonde = print("\U0001F931")
+normal_person = print("\U0001F9CD")
+
+#OTHER CHARACTERS: 
+ikea_construction_worker = print("\U0001F477")
+ikea_information_worker = print("\U0001F481")
+
+#THINGS TO COLLECT
+key = print("\U0001F511")
+
+baby_angel_head = print("\U0001F47C")
+baby_bottle = print("\U0001F37C")
+purse = print("\U0001F45C")
+
+shopping_trolley = print("\U0001F6D2")
+pencil = print("\U0000270F")
+
+frying_pan = print("\U0001F373")
+knife = print("\U0001F52A")
+
+broccoli = print("\U0001F966")
+hot_dog = print("\U0001F32D")
+meat_balls = print("\U0001F9C6")
+ice_cream = print("\U0001F366")
+
+#BOSS has to be bigger, so we go with head, torso and shoe
+head_with_beard = print("\U0001F9D4")
+t_shirt = print("\U0001F455")
+shorts = print("\U0001F456")
+
+
