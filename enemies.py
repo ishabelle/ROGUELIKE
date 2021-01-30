47a8eb86c6f1156e939e257ca97bd0349e7ae2f0
Monika Kubowicz <monkubowicz@gmail.com>
2021-01-30 17:21:03 +0100

    Put coordinates to enemies on all levels

--- enemies.py
+++ enemies.py
@@ -14,7 +14,7 @@ def create_ikea_client(x_position, y_position):
     enemy = {}
     enemy["x"] = x_position
     enemy["y"] = y_position
-    enemy["icon"] = "\033[1;31;1m&\033[1;37;1m"
+    enemy["icon"] = "\033[1;31;1m%\033[1;37;1m"
     enemy["type"] = "y_move"
     enemy["direction"] = "up"
     enemy["attack"] = 10
@@ -23,12 +23,11 @@ def create_ikea_client(x_position, y_position):
 
 
 def create_enemies_list(file_name):
-    # sprawdzić indeksy na planszach , czy mieszczą się na mapach txt (wpisałam randomowe wartości)
     if file_name == "levels/map_level1.txt":
         X_INDEX = 0
         Y_INDEX = 1
-        ikea_worker_coordinates = [(11, 13)]
-        ikea_client_coordinates = [(20, 4)]
+        ikea_worker_coordinates = [(21, 10)]
+        ikea_client_coordinates = [(52, 7)]
         ikea_worker_enemy = [create_ikea_worker(element[X_INDEX], element[Y_INDEX]) for element in ikea_worker_coordinates]
         ikea_client_enemy = [create_ikea_client(element[X_INDEX], element[Y_INDEX]) for element in ikea_client_coordinates]
         enemies_list = ikea_worker_enemy + ikea_client_enemy
@@ -36,8 +35,8 @@ def create_enemies_list(file_name):
     elif file_name == "levels/map_level2.txt":
         X_INDEX = 0
         Y_INDEX = 1
-        ikea_worker_coordinates = [(1, 4)]
-        ikea_client_coordinates = [(5, 5)]
+        ikea_worker_coordinates = [(23, 5)]
+        ikea_client_coordinates = [(58, 2)]
         ikea_worker_enemy = [create_ikea_worker(element[X_INDEX], element[Y_INDEX]) for element in ikea_worker_coordinates]
         ikea_client_enemy = [create_ikea_client(element[X_INDEX], element[Y_INDEX]) for element in ikea_client_coordinates]
         enemies_list = ikea_worker_enemy + ikea_client_enemy
@@ -45,8 +44,8 @@ def create_enemies_list(file_name):
     elif file_name == "levels/map_level3.txt":
         X_INDEX = 0
         Y_INDEX = 1
-        ikea_worker_coordinates = [(33, 4)]
-        ikea_client_coordinates = [(3, 10)]
+        ikea_worker_coordinates = [(21, 4)]
+        ikea_client_coordinates = [(38, 11)]
         ikea_worker_enemy = [create_ikea_worker(element[X_INDEX], element[Y_INDEX]) for element in ikea_worker_coordinates]
         ikea_client_enemy = [create_ikea_client(element[X_INDEX], element[Y_INDEX]) for element in ikea_client_coordinates]
         enemies_list = ikea_worker_enemy + ikea_client_enemy
