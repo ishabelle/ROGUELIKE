bc9167661884003b54cc11de8332af5da8eef38e
ishabelle <kolodzinska.izabela@gmail.com>
2021-01-30 12:18:23 +0100

    Creating items to collect during game

---
+++ items_to_collect.py
@@ -0,0 +1,154 @@
+def create_key(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F511"
+    item["type"] = "key"
+    item["HP"] = 0
+    item["on_board"] = 1 
+    item["weight"] = 5
+    return item
+
+
+def create_baby_angel_head(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F47C"
+    item["type"] = "collection"
+    item["HP"] = 5
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_baby_bottle(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F37C"
+    item["type"] = "collection"
+    item["HP"] = 10
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_purse(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F45C"
+    item["type"] = "collection"
+    item["HP"] = 15
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_shopping_bags(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F6CD"
+    item["type"] = "collection"
+    item["HP"] = 5
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_shopping_trolley(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F6D2"
+    item["type"] = "collection"
+    item["HP"] = 15
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_pencil(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0000270F"
+    item["type"] = "collection"
+    item["HP"] = 10
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_frying_pan(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F373"
+    item["type"] = "weapon"
+    item["HP"] = 20
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_knife(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F52A"
+    item["type"] = "weapon"
+    item["HP"] = 15
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_broccoli(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F966"
+    item["type"] = "food"
+    item["HP"] = 10
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item
+
+
+def create_hot_dog(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F32D"
+    item["type"] = "food"
+    item["HP"] = 15
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item   
+
+
+def create_meat_balls(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F9C6"
+    item["type"] = "food"
+    item["HP"] = 20
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item 
+
+
+def create_ice_cream(x_position, y_position):
+    item = {}
+    item["x"] = x_position
+    item["y"] = y_position
+    item["icon"] = "\U0001F366"
+    item["type"] = "food"
+    item["HP"] = 25
+    item["on_board"] = 1
+    item["weight"] = 5
+    return item 
