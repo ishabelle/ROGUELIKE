86b89b0cf1735592c29927ad2b62e63c451f02c9
ishabelle <kolodzinska.izabela@gmail.com>
2021-01-30 13:17:38 +0100

    Create enemies list in second level maps

--- enemies.py
+++ enemies.py
@@ -47,6 +47,7 @@ def create_ikea_detective(x_position, y_position):
 
 
 def create_enemies_list(file_name):
+    # sprawdzić indeksy na planszach , czy mieszczą się na mapach txt (wpisałam randomowe wartości)
     if file_name == "levels/map_level1.txt":
         X_INDEX = 0
         Y_INDEX =1
@@ -60,7 +61,18 @@ def create_enemies_list(file_name):
         ikea_detective_enemy = [create_ikea_detective(element[X_INDEX], element[Y_INDEX]) for element in ikea_detective_coordinates]
         enemies_list = ikea_contruction_worker_enemy + ikea_information_worker_enemy + ikea_bodyguard_enemy + ikea_detective_enemy
     elif file_name == "levels/map_level2.txt":
-        pass
+        X_INDEX = 0
+        Y_INDEX =1
+        ikea_contruction_worker_coordinates = [(12, 19)]
+        ikea_information_worker_coordinates = [(34, 10)]
+        ikea_bodyguard_coordinates = [(43, 32)]
+        ikea_detective_coordinates = [(9, 5)]
+        ikea_contruction_worker_enemy = [create_ikea_construction_worker(element[X_INDEX], element[Y_INDEX]) for element in ikea_contruction_worker_coordinates]
+        ikea_information_worker_enemy = [create_ikea_information_worker(element[X_INDEX], element[Y_INDEX]) for element in ikea_information_worker_coordinates]
+        ikea_bodyguard_enemy = [create_ikea_bodyguard(element[X_INDEX], element[Y_INDEX]) for element in ikea_bodyguard_coordinates]
+        ikea_detective_enemy = [create_ikea_detective(element[X_INDEX], element[Y_INDEX]) for element in ikea_detective_coordinates]
+        enemies_list = ikea_contruction_worker_enemy + ikea_information_worker_enemy + ikea_bodyguard_enemy + ikea_detective_enemy
+        
     elif file_name == "levels/map_level3.txt":
         pass
     return enemies_list
